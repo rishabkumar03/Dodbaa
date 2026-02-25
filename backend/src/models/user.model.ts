5cbeff0a49024a8ae2523718c2919fc52f844924
rishabkumar03 <rishabkumar3.2005@gmail.com>
2026-02-25 12:47:46 +0530

    Implement user authentication with JWT and password hashing; update user schema validation

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,4 +1,7 @@
 import mongoose, { Schema, Document } from "mongoose";
+import jwt from "jsonwebtoken"
+import type { SignOptions } from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 export interface UserDocument extends Document {
     fullname: string;
@@ -6,9 +9,12 @@ export interface UserDocument extends Document {
     password: string;
     phone: string;
     role: "user" | "artist" | "admin";
-    address: mongoose.Types.ObjectId;
     resetPasswordOTP?: string;
     resetPasswordOTPExpiry?: Date;
+
+    isPasswordCorrect(password: string): Promise<boolean>;
+    generateAccessToken(): string;
+    generateRefreshToken(): string;
 }
 
 const UserDbSchema: Schema<UserDocument> = new Schema({
@@ -36,15 +42,56 @@ const UserDbSchema: Schema<UserDocument> = new Schema({
         enum: ["user", "artist", "admin"],
         default: "user"
     },
-    address: {
-        type: Schema.Types.ObjectId,
-        trim: true,
-        ref: "AddressModel",
-        required: true
-    },
     resetPasswordOTP: String,
     resetPasswordOTPExpiry: Date,
 
 }, { timestamps: true })
+
+UserDbSchema.pre<UserDocument>("save", async function (this: UserDocument, next: any){
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 6)
+    next()
+})
+
+UserDbSchema.methods.isPasswordCorrect = async function (password: string){
+    return await bcrypt.compare(password, this.password)
+}
+
+UserDbSchema.methods.generateAccessToken = function(){
+    const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as string;
+    if (!accessTokenSecret) {
+        throw new Error("ACCESS_TOKEN_SECRET is not defined");
+    }
+    const accessTokenOptions: SignOptions = {
+        expiresIn: (process.env.ACCESS_TOKEN_EXPIRY as string) || "3h"
+    };
+    return jwt.sign(
+        {
+            _id: this._id,
+            fullname: this.fullname,
+            email: this.email 
+        },
+        accessTokenSecret,
+        accessTokenOptions
+    )
+}
+
+UserDbSchema.methods.generateRefreshToken = function(){
+    const refereshTokenSecret = process.env.REFRESH_TOKEN_SECRET as string;
+    if (!refereshTokenSecret) {
+        throw new Error("REFRESH_TOKEN_SECRET is not defined");
+    }
+    const referenceTokenOptions: SignOptions = {
+        expiresIn: (process.env.REFRESH_TOKEN_EXPIRY as string) || "7d"
+    }
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        refereshTokenSecret,
+        referenceTokenOptions
+    )
+}
 
 export const UserModel = mongoose.model<UserDocument>("UserModel", UserDbSchema);
