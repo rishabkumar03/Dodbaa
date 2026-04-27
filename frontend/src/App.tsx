b92dff76455707ce8b28d411fa33b5fd7e3d3a20
RohitMunda07 <rohitkumar14818@gmail.com>
2026-04-27 22:21:20 +0530

    feat: implement routing in App component with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import ProductDetailPage from './pages/ProductDetailPage'
 import ProductsPage from './pages/ProductsPage'
 import ProfilePage from './pages/ProfilePage'
 import WishlistPage from './pages/WishlistPage'
-
+import { Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   const [nav, setNav] = useState({ page: 'home', id: undefined as string | undefined })
@@ -25,14 +25,16 @@ function App() {
 
   return (
     <>
-      <HomePage onNavigate={handleNavigate} />
-      <AboutPage onNavigate={handleNavigate} />
-      <OrderPage onNavigate={handleNavigate} />
-      <CartPage onNavigate={handleNavigate} />
-      <ProductsPage onNavigate={handleNavigate} />
-      {/* <ProductDetailPage onNavigate={handleNavigate1} /> */}
-      <ProfilePage onNavigate={handleNavigate} />
-      <WishlistPage onNavigate={handleNavigate} />
+      <Routes>
+        <Route path='/' element={<HomePage onNavigate={handleNavigate} />} />
+        <Route path='/about' element={<AboutPage onNavigate={handleNavigate} />} />
+        <Route path='/order' element={<OrderPage onNavigate={handleNavigate} />} />
+        <Route path='/cart' element={<CartPage onNavigate={handleNavigate} />} />
+        <Route path='/products' element={<ProductsPage onNavigate={handleNavigate} />} />
+        <Route path='/profile' element={<ProfilePage onNavigate={handleNavigate} />} />
+        <Route path='/wishlist' element={<WishlistPage onNavigate={handleNavigate} />} />
+        {/* <Route path='/wishlist' element={{ < ProductDetailPage onNavigate={handleNavigate1} /> }} /> */}
+      </Routes>
       <CookieBanner />
     </>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/index.ts'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import HomePage from './pages/HomePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store} >
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
